d2213730959755714233634fa385811d5e7021d6
iqbaalgifari <iqbalalghifari18283@gmail.com>
2025-07-12 16:13:51 +0700

    feat: fixed useSerachParams()

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
-import Image from "next/image";
 import BannerComponent from "./components/BannerComponent";
 import BlogComponent from "./components/BlogComponent";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <BannerComponent/>
       <BlogComponent/>
-    </>
+    </Suspense>
   );
 }
